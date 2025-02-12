53156f230dd961b50a621808e2bdef648968fed4
GiorgioDalla <giorgiodallagoiv@hotmail.com>
2025-02-12 18:22:44 +0100

    chimera setup

--- contracts/utils/fundAccounts.js
+++
@@ -1,9 +0,0 @@
-const fundAccounts = async (accounts, token) => {
-  return Promise.all(
-    accounts.map((account) => (
-      token.mint(account, String(10n ** 36n))
-    )),
-  );
-};
-
-module.exports = { fundAccounts };
